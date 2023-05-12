57a80968cd326ad9b9f42c1aad9e222ae7434836
Chris Openshaw <copenshaw17@gmail.com>
2023-05-12 12:48:14 -0500

    More gui changes to help visualize

--- Youmix/editor/editorgui.py
+++ Youmix/editor/editorgui.py
@@ -10,21 +10,31 @@ from typing import Optional, List
 class EditWindow(tk.Tk):
     def __init__(self, streams: Optional[List[str]] = None) -> None:
         super().__init__()
+        self.edit_streams = streams
 
         #main window
         self.title("Editor")
         self.geometry("600x300")
         self.resizable(False,False)
 
+        #Control bar
+        self.control_frame = Frame(self, height=50, bg="black")
+
         #Audio list
-        self.edit_streams = streams
-        self.edit_list = Listbox(self, selectmode=EXTENDED)
-        for stream in self.edit_streams:
-            self.edit_list.insert(END, stream)
+        self.streams_frame = Frame(self, bg="blue")
+        self.edit_list = Listbox(self.streams_frame, selectmode=EXTENDED)
+        if streams is None:
+            pass
+        else:
+            for stream in self.edit_streams:
+                self.edit_list.insert(END, stream)
 
         #Packing
-        self.edit_list.pack(side=LEFT)
+        self.control_frame.pack(side=TOP, fill=X)
+        self.streams_frame.pack(side=TOP, expand=TRUE, fill=BOTH)
+        self.edit_list.pack(side=LEFT, fill=Y)
 
 if __name__ == "__main__":
-    app = EditWindow()
+    _dummy_list = ["dummy_stream" for i in range(10)]
+    app = EditWindow(_dummy_list)
     app.mainloop()
